837a59b94fbe34bf5d47621760185251e8dfeacb
kingsleyz <kylehorton33@gmail.com>
2021-07-12 23:34:39 +0100

    homepage layout - plant watcher

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,31 @@
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+    },
+    themes: {
+        light: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+        dark: {
+          primary: '#2196F3',
+          secondary: '#424242',
+          accent: '#FF4081',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+    },
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-avatar class="primary mx-2" size="40">
+        <span class="white--text">KH</span>
+      </v-avatar>
+      <v-toolbar-title>Plant Watcher</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ username }}'s
+            Plant Watcher
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <!-- -->
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container fluid>
+        <v-row dense>
+          <v-col
+            v-for="plant in plants"
+            :key="plant.id"
+            cols="6"
+            md="2"
+          >
+            <v-card elevation="3">
+              <v-img
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                :src="plant.latest_pic"
+                height="200px"
+              >
+                <v-card-title v-text="plant.name.toUpperCase()"></v-card-title>
+              </v-img>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon class="success--text">mdi-emoticon-happy</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon class="error--text">mdi-emoticon-sad</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-camera</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    data: () => ({ 
+      drawer: null,
+      username: 'kingsley',
+      items: [
+        { title: 'Oldest', icon: 'mdi-clock-outline' },
+        { title: 'Add', icon: 'mdi-plus' },
+        { title: 'About', icon: 'mdi-help-box' },
+      ],
+      plants: [
+        {id: 1, name: 'hosta', latest_pic: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F06%2F10%2Ffrancee-hosta-purple-flowers-562666cc.jpg'},
+        {id: 2, name: 'hosta', latest_pic: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F06%2F10%2Ffrancee-hosta-purple-flowers-562666cc.jpg'},
+        {id: 3, name: 'hosta', latest_pic: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F06%2F10%2Ffrancee-hosta-purple-flowers-562666cc.jpg'},
+      ],
+    }),
   }
-}
 </script>
