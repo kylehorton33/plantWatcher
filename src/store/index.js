b001075944466c4e58293e1c2d1f3d125e0436cb
kingsley-hp <kylehorton33@gmail.com>
2021-07-14 20:02:37 -0400

    json-server, axios get for all plants

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
 Vue.use(Vuex)
@@ -47,36 +48,11 @@ export default new Vuex.Store({
   actions: {
     loadPlants ({commit}) {
       commit('setLoading', true)
-      const PLANTS = [
-        { id: 'new',
-          name: '',
-          updated_at: null,
-          location: '',
-          latest_pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ZqqJYrgFspbx-gGFudtl4wHaHa%26pid%3DApi&f=1'
-        },
-        { id: '51a73e58-44e3-4756-a60c-597451dbb588',
-          added_at: 1626140625,
-          updated_at: 1626140625,
-          name: 'hosta',
-          location: 'inside',
-          latest_pic: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F06%2F10%2Ffrancee-hosta-purple-flowers-562666cc.jpg'
-        },
-        { id: '6175de1d-d79e-4606-841a-77c3e7ba734f',
-          added_at: 1626140640,
-          updated_at: 1626140640,
-          name: 'peace lily',
-          location: 'inside',
-          latest_pic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Froadrunnerflorist.com%2Fwp-content%2Fuploads%2F2014%2F10%2Fpeace-lily-sept..jpg'
-        },
-        { id: '0d1ec9f6-a8aa-4065-bf3b-039a090e68cb',
-          added_at: 1626040625,
-          updated_at: 1626040625,
-          name: 'aloe',
-          location: 'inside',
-          latest_pic: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0267%2F1299%2F7940%2Fproducts%2F1600974242_1200x1200.jpg'
-        },
-      ]
-      commit('setLoadedPlants', PLANTS)
+      axios.get('http://localhost:3000/plants')
+        .then((res) => {
+          commit('setLoadedPlants', res.data)
+        })
+        .catch((err) => console.log(err))
       commit('setLoading', false)
     },
     addPlant({commit}, payload) {
